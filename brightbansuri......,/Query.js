72b26575cad1d4b677b67f38ff35db3d2ff56564
Gseha <gseha3228@gmail.com>
2021-04-17 12:55:24 +0530

    Added bansuri folder

---
+++ brightbansuri......,/Query.js
@@ -0,0 +1,10 @@
+
+var fname = document.getElementById("firstname");
+
+function alphabet(){
+    alert( "welcome "+  fname.value);
+    console.log(fname.value)
+}
+function printing(){
+    alert( fname.value)
+}
